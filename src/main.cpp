a054f7500dca4e38a604f20309854582328b1125
Ninagail <119610609+Ninagail@users.noreply.github.com>
2024-02-03 15:59:27 +0100

    balle bouge dans carré

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #define DOCTEST_CONFIG_IMPLEMENT
+#include <random>
 #include "doctest/doctest.h"
 #include "p6/p6.h"
 
@@ -13,12 +14,28 @@ int main()
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
     ctx.maximize_window();
 
+    float x_pos   = p6::random::number(-1.0f, 1.0f);
+    float y_pos   = p6::random::number(-1.0f, 1.0f);
+    float x_speed = p6::random::number(-0.01f, 0.01f);
+    float y_speed = p6::random::number(-0.01f, 0.01f);
     // Declare your infinite update loop.
     ctx.update = [&]() {
+        x_pos += x_speed;
+        y_pos += y_speed;
+        if (x_pos <= -0.95f || x_pos >= 0.95f)
+        {
+            x_speed = -x_speed;
+        }
+        if (y_pos <= -0.95f || y_pos >= 0.95f)
+        {
+            y_speed = -y_speed;
+        }
+
         ctx.background(p6::NamedColor::BabyBlue);
+        // ctx.translate(x, y);
         ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.1f}
+            p6::Center{x_pos, y_pos},
+            p6::Radius{0.05f}
         );
         ctx.square(
             p6::Center{0.0f, 0.0f},
